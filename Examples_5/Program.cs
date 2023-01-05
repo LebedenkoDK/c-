0f852d52d76b8cda9fdf224b9227dcb4abe93de8
Dima <nec72@mail.ru>
2023-01-05 21:47:04 +0300

    Сделали приветсвие с выделением имеми на языке c#

---
+++ Examples_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дмитрий")
+{
+    Console.WriteLine("Ура, это же Димка");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
