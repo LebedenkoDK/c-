003a9867079b9b54ee4abe369b4ebb2fa8820f95
Dima <nec72@mail.ru>
2023-01-07 16:19:23 +0300

    Сделал первое ДЗ по первому семинару

---
+++ homework_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = -9;
+int b = -3;
+
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
